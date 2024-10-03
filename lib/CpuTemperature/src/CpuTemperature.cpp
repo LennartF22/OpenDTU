21218ccd933b1aca347d5fb4a2fd1d974632d702
LennartF22 <18723691+LennartF22@users.noreply.github.com>
2024-10-04 01:07:57 +0200

    Fix CpuTemperature for newer ESP-IDF versions

--- lib/CpuTemperature/src/CpuTemperature.cpp
+++ lib/CpuTemperature/src/CpuTemperature.cpp
@@ -6,13 +6,17 @@
 #include "CpuTemperature.h"
 #include <Arduino.h>
 
-#if defined(CONFIG_IDF_TARGET_ESP32)
+#if CONFIG_IDF_TARGET_ESP32
 // there is no official API available on the original ESP32
 extern "C" {
 uint8_t temprature_sens_read();
 }
-#elif defined(CONFIG_IDF_TARGET_ESP32C3) || defined(CONFIG_IDF_TARGET_ESP32S2) || defined(CONFIG_IDF_TARGET_ESP32S3)
+#elif CONFIG_IDF_TARGET_ESP32C3 || CONFIG_IDF_TARGET_ESP32S2 || CONFIG_IDF_TARGET_ESP32S3
+#if ESP_IDF_VERSION_MAJOR < 5
 #include "driver/temp_sensor.h"
+#else
+#include "driver/temperature_sensor.h"
+#endif
 #endif
 
 CpuTemperatureClass CpuTemperature;
@@ -24,22 +28,30 @@ float CpuTemperatureClass::read()
     float temperature = NAN;
     bool success = false;
 
-#if defined(CONFIG_IDF_TARGET_ESP32)
+#if CONFIG_IDF_TARGET_ESP32
     uint8_t raw = temprature_sens_read();
     ESP_LOGV(TAG, "Raw temperature value: %d", raw);
     temperature = (raw - 32) / 1.8f;
     success = (raw != 128);
-#elif defined(CONFIG_IDF_TARGET_ESP32C3) || defined(CONFIG_IDF_TARGET_ESP32S2) || defined(CONFIG_IDF_TARGET_ESP32S3)
+#elif CONFIG_IDF_TARGET_ESP32C3 || CONFIG_IDF_TARGET_ESP32S2 || CONFIG_IDF_TARGET_ESP32S3
+#if ESP_IDF_VERSION_MAJOR < 5
     temp_sensor_config_t tsens = TSENS_CONFIG_DEFAULT();
     temp_sensor_set_config(tsens);
     temp_sensor_start();
-#if defined(CONFIG_IDF_TARGET_ESP32S3) && (ESP_IDF_VERSION < ESP_IDF_VERSION_VAL(4, 4, 3))
-#error \
-    "ESP32-S3 internal temperature sensor requires ESP IDF V4.4.3 or higher. See https://github.com/esphome/issues/issues/4271"
-#endif
     esp_err_t result = temp_sensor_read_celsius(&temperature);
     temp_sensor_stop();
     success = (result == ESP_OK);
+#else
+    static temperature_sensor_handle_t sensor_handle = nullptr;
+    if (sensor_handle == nullptr) {
+        // Range with 1°C accuracy
+        temperature_sensor_config_t sensor_config = TEMPERATURE_SENSOR_CONFIG_DEFAULT(-10, 80);
+        ESP_ERROR_CHECK(temperature_sensor_install(&sensor_config, &sensor_handle));
+    }
+    ESP_ERROR_CHECK(temperature_sensor_enable(sensor_handle));
+    success = (temperature_sensor_get_celsius(sensor_handle, &temperature) == ESP_OK);
+    ESP_ERROR_CHECK(temperature_sensor_disable(sensor_handle));
+#endif
 #endif
 
     if (success && std::isfinite(temperature)) {
